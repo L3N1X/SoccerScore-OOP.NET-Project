4f57bdcb204522b44ad2610bcff9cab48340d699
Leon Kruslin <leon.kruslin@gmail.com>
2022-07-10 15:27:46 +0200

    Adding national team window

--- SoccerScore-WPF edition/NationalTeamWindow.xaml.cs
+++ SoccerScore-WPF edition/NationalTeamWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using SoccerScoreData.Dal;
+using SoccerScoreData.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,9 +22,62 @@ namespace SoccerScore_WPF_edition
     public partial class NationalTeamWindow : Window
     {
         private readonly DataManager _dataManager;
-        public NationalTeamWindow()
+        private readonly string _fifaCode;
+
+        private NationalTeam _nationalTeam;
+        public NationalTeamWindow(DataManager dataManager, string fifaCode)
         {
+            _dataManager = dataManager;
+            _fifaCode = fifaCode;
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            InitializeControls();
+        }
+
+        private async void InitializeControls()
+        {
+            SetVisibilityForLoading();
+
+            if (_fifaCode.Equals(_dataManager.FavouriteTeam.FifaCode))
+                _nationalTeam = _dataManager.FavouriteTeam;
+            else
+            {
+                var teams = await _dataManager.GetSelectionTeams();
+                _nationalTeam = teams.Where(team => _fifaCode.Equals(team.FifaCode)).ToList()[0];
+            }
+            FillTeamData();
+
+            SetVisibiltyAfterLoading();
+        }
+
+        private void FillTeamData()
+        {
+            this.lblFifaCode.Content = _nationalTeam.FifaCode;
+            this.lblDraws.Content = _nationalTeam.Draws;
+            this.lblGamesPlayed.Content = _nationalTeam.GamesPlayed;
+            this.lblGoalDifferential.Content = _nationalTeam.GoalDifferential;
+            this.lblGoalsAgainst.Content = _nationalTeam.GoalsAgainst;
+            this.lblGoalsFor.Content = _nationalTeam.GoalsFor;
+            this.lblLosses.Content = _nationalTeam.Losses;
+            this.lblTotalPoints.Content = _nationalTeam.Points;
+            this.lblWins.Content = _nationalTeam.Wins;
+        }
+
+        private void SetVisibiltyAfterLoading()
+        {
+            this.pnlLoading.Visibility = Visibility.Hidden;
+            this.pnlFlag.Visibility = Visibility.Visible;
+            this.pnlTeamData.Visibility = Visibility.Visible;
+        }
+
+        private void SetVisibilityForLoading()
+        {
+            this.pnlLoading.Visibility = Visibility.Visible;
+            this.pnlFlag.Visibility = Visibility.Hidden;
+            this.pnlTeamData.Visibility = Visibility.Hidden;
         }
     }
 }
